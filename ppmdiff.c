fc4e9115e3225f72469fc759596b2435414e5a7f
oliuy1014 <oliveruy11@gmail.com>
2023-03-01 10:47:53 -0500

    ppmdiff implemented with correct flowers output, need to add check for images of width or height diff > 1

--- ppmdiff.c
+++ ppmdiff.c
@@ -2,59 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <math.h>
 #include "a2plain.h"
 #include "uarray2.h"
 #include "pnm.h"
 
 void usage(char *progname);
 FILE *open_file(char *filename, char *mode);
-
+double diff_score(Pnm_ppm img_1, Pnm_ppm img_2, A2Methods_T methods);
+void set_files(FILE **fp_1, FILE **fp_2, char *filename_1, char *filename_2);
+int min(int a, int b);
+void trim_file(A2Methods_UArray2 pixmap, int w, int h, A2Methods_T methods);
 
 int main(int argc, char *argv[])
 {
+        /* Checking args */
         if (argc != 3) {
-                fprintf(stderr, "Incorrect number of arguments.");
+                fprintf(stderr, "Invalid number of arguments.");
                 usage(argv[0]);
         }
         if ((strcmp(argv[1], "-") == 0 && (strcmp(argv[2], "-") == 0))) {
                 fprintf(stderr, "At most one input filename can be stdin.\n");
                 usage(argv[0]);
         }
-        FILE *fp_1 = open_file(argv[1], "rb");
-        FILE *fp_2 = open_file(argv[1], "rb");
+
+        FILE *fp_1 = NULL;
+        FILE *fp_2 = NULL;
+        set_files(&fp_1, &fp_2, argv[1], argv[2]);
+
         A2Methods_T methods = uarray2_methods_plain;
         Pnm_ppm img_1 = Pnm_ppmread(fp_1, methods);
         Pnm_ppm img_2 = Pnm_ppmread(fp_2, methods);
         fclose(fp_1);
         fclose(fp_2);
-
-        A2Methods_UArray2 pix_1 = img_1->pixels;
-        A2Methods_UArray2 pix_2 = img_2->pixels;
-
-        int pix1w = methods->width(pix_1);
-        int pix2w = methods->width(pix_2);
-        int pix1h = methods->height(pix_1);
-        int pix2h = methods->height(pix_2);
-
-        printf("pix1h = %d, pix1w = %d, pix2h = %d, pix2w = %d\n", pix1h, pix1w, pix2h, pix2w);
-
-        if (abs(methods->width(pix_1) - methods->width(pix_2)) > 1 || 
-            abs(methods->height(pix_1) - methods->height(pix_2)) > 1) {
-                fprintf(stderr, "Height or width of given images \
-                        differ by more than 1 pixel.");
-                printf("1\n");
-        }
-        
-        /* make apply function to do the math with pixels
-         * apply maps through pix_1, val = pixel inside pix_1
-         * cl has to hold pix_2, val_2 = methods->at(pix_2, i, j)
-         * and cl has to hold E value to be updated
-         * apply has to go inside val->red, green, blue and 
-         * edge case: different bounds of images? 
-         */
-        
+        /* TODO: check if width or height diff greater than 1 */
+        double diff = diff_score(img_1, img_2, methods);
+        printf("diff: %f\n", diff);
         Pnm_ppmfree(&img_1);
         Pnm_ppmfree(&img_2);
+        return 0;
 }
 
 void usage(char *progname)
@@ -78,4 +64,51 @@ FILE *open_file(char *filename, char *mode)
                 }
         }
         return returnfp;
+}
+
+double diff_score(Pnm_ppm img_1, Pnm_ppm img_2, A2Methods_T methods)
+{
+        assert(img_1 != NULL);
+        assert(img_2 != NULL);
+        assert(methods != NULL);
+        int width = min(img_1->width, img_2->width);
+        int height = min(img_1->height, img_2->height);
+        double denom_1 = (double) img_1->denominator;
+        double denom_2 = (double) img_2->denominator;
+        double diff = 0;
+        for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                        Pnm_rgb p1 = methods->at(img_1->pixels, i, j);
+                        Pnm_rgb p2 = methods->at(img_2->pixels, i, j);
+                        double r = (double) (p1->red / denom_1) - 
+                                   (double) (p2->red / denom_2);
+                        double g = (double) (p1->green / denom_1) - 
+                                   (double) (p2->green / denom_2);
+                        double b = (double) (p1->blue / denom_1) - 
+                                   (double) (p2->blue / denom_2);
+                        diff += ((r * r) + (g * g) + (b * b));
+                }
+        }
+        double ret = sqrt(diff / (3 * width * height));
+        fprintf(stderr, "diff: %f\n", ret);
+        return ret;
+}
+
+void set_files(FILE **fp_1, FILE **fp_2, char *filename_1, char *filename_2) 
+{
+        if (strcmp(filename_1, "-") == 0) {
+                *fp_1 = stdin;
+        } else {
+                *fp_1 = open_file(filename_1, "rb");
+        }
+        if (strcmp(filename_2, "-") == 0) {
+                *fp_2 = stdin;
+        } else {
+                *fp_2 = open_file(filename_2, "rb");
+        }
+}
+
+int min(int a, int b) 
+{
+        return a < b ? a : b;
 }
